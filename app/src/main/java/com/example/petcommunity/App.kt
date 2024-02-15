e294e7e72dcff20f48bef362ce363380b3aab7f9
NGAPC\hqnga <hqnga1604@gmail.com>
2024-02-15 16:00:36 +0700

    test1

--- app/src/main/java/com/example/petcommunity/App.kt
+++ app/src/main/java/com/example/petcommunity/App.kt
@@ -8,27 +8,5 @@ import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class App: Application() {
-    override fun onConfigurationChanged(newConfig: Configuration) {
-        super.onConfigurationChanged(newConfig)
-    }
 
-    override fun onLowMemory() {
-        super.onLowMemory()
-
-    }
-
-    override fun onTrimMemory(level: Int) {
-        super.onTrimMemory(level)
-
-    }
-
-    override fun onCreate() {
-        super.onCreate()
-
-    }
-
-    override fun onTerminate() {
-        super.onTerminate()
-
-    }
 }
